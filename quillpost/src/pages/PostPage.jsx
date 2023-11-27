4b7efa65423df16728f1522390610275ea1c347a
Kunal <kunalsingh2703@gmail.com>
2023-11-27 16:30:33 +0530

    Completed Blog Posting Page

--- quillpost/src/pages/PostPage.jsx
+++ quillpost/src/pages/PostPage.jsx
@@ -5,6 +5,7 @@ import {Link, useParams} from "react-router-dom"
 import {Row, Col, Container} from "reactstrap"
 import { loadPost } from '../services/post-service'
 import {toast} from "react-toastify"
+import { Card, CardBody, CardText } from 'reactstrap';
 const PostPage = () => {
 
 
@@ -21,17 +22,51 @@ const PostPage = () => {
             toast.error("Error in loading post")
         })
     }, [])
+
+    const printDate = (number) => {
+        return new Date(number).toLocaleDateString()
+    }
+
   return (
     <Base>
         <div>
             <Container className="mt-4">
-                    <Link to = "/">Home</Link>
+                    <Link to = "/">Home</Link> / {post && (<Link to = "">{post.title}</Link>)}
                     <Row>
                         <Col md = {{
                             size: 12
                         }}> 
 
-                        <h1>Load the post from database</h1>
+                            <div>
+                                <Card className="mt-3 ps-2 border-0 shadow-sm" >
+                                    {
+                                        (post) && (
+                                            <CardBody>
+                                                <CardText> Posted By <b>{post.username}</b> on <b>{ printDate(post.date)}</b> </CardText>
+
+                                                    <CardText>
+                                                        <span className="text-muted">Category: {post.categoryName}</span>
+                                                    </CardText>
+
+                                                    <div className="divder" style={{
+                                                        width: '100%',
+                                                        height: '1px',
+                                                        background: '#e2e2e2'
+                                                    }}></div>
+
+                                                    <CardText className="mt-3">
+                                                        <h1>{post.title}</h1>
+                                                    </CardText>
+                                                    
+
+                                                    <CardText dangerouslySetInnerHTML={{__html: post.content}}>
+
+                                                    </CardText>
+                                            </CardBody>
+                                        )
+                                    }
+                                </Card>
+                            </div>
 
 
 
